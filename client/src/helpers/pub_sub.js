742cfeb9c9c999fad32f7337ad7495a3f655b9ad
Simon Shephard <simonshephard@hotmail.com>
2018-10-28 11:23:45 +0000

    refactors class, imports, and destructures family data function

---
+++ client/src/helpers/pub_sub.js
@@ -0,0 +1,12 @@
+export const PubSub = {
+  publish: function (channel, payload) {
+    var event = new CustomEvent(channel, {
+      detail: payload
+  });
+    document.dispatchEvent(event);
+  },
+
+  subscribe: function (channel, callback) {
+    document.addEventListener(channel, callback);
+  }
+};
